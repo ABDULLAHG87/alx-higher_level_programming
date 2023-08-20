a649fee06c9af67518def28c2d28bb124883175b
Abdulhakeem Abdullahi <hakeemabdullah87@gmail.com>
2023-08-20 15:23:41 -0700

    refactor task 2 code without using the list comprehension

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute(f"SELECT * FROM `states` WHERE BINARY `name` ='{sys.argv[4]}'")
-    [print(state) for state in cur.fetchall()]
+    for state in cur.fetchall():
+        print(state)
