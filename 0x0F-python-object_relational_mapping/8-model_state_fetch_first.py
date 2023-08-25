7092cd9387c63321aab6382d910d93ca949e296f
Abdulhakeem Abdullahi <hakeemabdullah87@gmail.com>
2023-08-25 16:59:53 -0700

    Refactoring task 8 code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,15 +6,20 @@ A Script that prins the first state object of the database hbtn_0e_6_us
 import sys
 import sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from module_state import State
+from module_state import Base,State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
     Session = sessionmakar(bind=engine)
     session = Session()
-
-    state - session.query(State).order_by(State.id).first()
+    
+    state = session.query(State).order_by(State.id).first()
     if state is None:
         print("Nothing")
     else:
-        print(f"{state.id}: {state.name})
+        print(f"{state.id}: {state.name}")
+
+    session.close()
