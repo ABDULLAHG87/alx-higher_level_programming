aa970f23ee068d376ed770d641551bdaa3dd8f36
Abdulhakeem Abdullahi <hakeemabdullah87@gmail.com>
2023-09-02 08:03:28 -0700

    Upload code for task 1 - 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-/*  script that prints 3 lines 
- * First Line: C is fun
- * Second Line: Python is cool
- * Third Line: JavaScript is amazing 
-*/
+
 console.log('C is fun\nPython is cool\nJavaScript is amazing');
 
 
