7eb9777dba7ad1187e45ee1030a55a0d4bb0260e
root <hakeemabdullah87@gmail.com>
2023-12-13 14:31:33 -0800

    BUG FIX: used the semistandard --fix to resolve error in the code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,11 +5,11 @@
  * @fileoverview
  */
 
-if (process.argv.length <= 3){
-    console.log(0);
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-    const args = process.argv.map(Number)
-	  .slice(2, process.argv.length);
-        .sort((a,b) => a - b);
-    console.log(args[args,length - 2]);
+  const args = process.argv.slice(2)
+    .map(Number)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
