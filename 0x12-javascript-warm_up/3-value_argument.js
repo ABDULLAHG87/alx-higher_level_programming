815c5072a969bcfe8d7da588a29834a7acdbd258
root <hakeemabdullah87@gmail.com>
2023-12-13 08:51:22 -0800

    REFACTOR: change code to ensure the standard linting rule is obeyed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-argType = typeof (process.argv[2]);
-
-if (argType === 'undefined') {
+if (typeof (process.argv[2]) === 'undefined') {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
