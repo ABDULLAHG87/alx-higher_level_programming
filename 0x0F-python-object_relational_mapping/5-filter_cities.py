85dc718e190463868fbf14c4a4ad49520c2f4d90
root <hakeemabdullah87@gmail.com>
2023-12-17 09:16:14 -0800

    REFACTOR: remove trailing whitespace at ln4, unexpected space around keyword ln11

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 '''
  Script to display all cities of ag iven state from the
- states table of the database 
+ states table of the database
 '''
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd= sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                   ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-    print(", ".join([city[2] for city in cur.fetchall() if city[4] == sys.argv[4]]))
+    print(", ".join([city[2] for city in cur.fetchall()
+                     if city[4] == sys.argv[4]]))
