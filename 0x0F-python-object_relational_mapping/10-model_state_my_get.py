382a2cf8189c69c4c3bcf30035b47f88b33f6ef3
root <hakeemabdullah87@gmail.com>
2023-12-17 13:12:00 -0800

    REFACTOR: restructured too long line ln(3 & 13)

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Task 10: A Script that lists the State object with the name passed as argument from the database hbtn_0e_6_usa.
+Task 10: A Script that lists the State object with the name passed.
 """
 
 import sys
@@ -10,13 +10,15 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    #creating a session
+    # creating a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    #declaring a boolean to detect present of name"
+    # declaring a boolean to detect present of name"
     found = False
     for state in session.query(State):
         if state.name == sys.argv[4]:
