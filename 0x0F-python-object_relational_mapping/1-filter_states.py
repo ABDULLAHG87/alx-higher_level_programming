7b5f1e6876493c434ae3b58818a59963730e6372
root <hakeemabdullah87@gmail.com>
2023-12-17 08:47:51 -0800

    REFACTOR: change code to comply with pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,14 +3,11 @@
 A script that list all states with a nme starting with N from data base
 '''
 
-#import libraries
 import sys
 import MySQLdb
 
-# Running Module as a Script
-
 if __name__ == "__main__":
-    #creating connection
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
