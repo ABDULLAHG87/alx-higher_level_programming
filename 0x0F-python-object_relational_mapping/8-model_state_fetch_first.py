70bb30ea4adf8ea0c0685d1423f593eaf5dbfa6d
root <hakeemabdullah87@gmail.com>
2023-12-17 07:56:45 -0800

    BUGFIX: fixing error within the source code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,18 +4,15 @@ A Script that prins the first state object of the database hbtn_0e_6_us
 """
 
 import sys
-from module_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session
+from module_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    #Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
     session = Session(engine)
     state = session.query(State).order_by(State.id).first()
 
