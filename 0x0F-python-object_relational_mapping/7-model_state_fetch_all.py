f4246f6b9283441b35b80462c16dfb2b4330b1be
root <hakeemabdullah87@gmail.com>
2023-12-17 06:54:26 -0800

    BUGFIX: fix code by editing corresponding import module

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,21 +2,17 @@
 '''
 Script that lists all states Object from the database htbn_0e_6_usa
 Module: SQLAlchemy
-
 '''
-
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import dedclarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session,sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine=create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                         .format(sys.argv[1], sys.argv[2], sys.argv[3],
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                                  pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id):
-        print(f"{state.id}: {state.name})
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
+    session.close()
