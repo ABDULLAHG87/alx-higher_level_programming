921d4407758a2b647fdf40eb35f2a7cc4c35f8d2
root <hakeemabdullah87@gmail.com>
2023-12-17 09:05:55 -0800

    REFACTOR: update the output section

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
+
     cur.execute("SELECT * FROM `states` WHERE `name` = '{}' ORDER BY `id`"
                 .format(sys.argv[4]))
     for state in cur.fetchall():
-        print(state)
+        if state == sys.argv[4]:
+            print(state)
