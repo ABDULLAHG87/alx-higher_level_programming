5fdf1b20153c30c9e1e33c0de6eb826bfbbfbb76
root <hakeemabdullah87@gmail.com>
2023-12-17 09:38:26 -0800

    REFACTOR: resolve E128 continuation line under-indented for visual indent error (Ln13)

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,13 +5,13 @@ Module: SQLAlchemy
 '''
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session,sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                 pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         print(f"{state.id}: {state.name}")
