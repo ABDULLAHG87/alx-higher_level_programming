c1aebb7d78d930765dbe7bae50fa60d65809a0e3
root <hakeemabdullah87@gmail.com>
2024-01-05 15:00:13 -0800

    BUGFIX: add newline after code

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, send a POST request ot passed URl
-curl -s -X POST -d "email:test@gmail.com&subject=I will always be here for PLD"\
-"$1"
+curl -s -X POST -d "email:test@gmail.com&subject=I will always be here for PLD" "$1"
