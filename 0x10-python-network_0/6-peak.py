d0e127cfad3a508cfaf9fd459faf10aa94785a9b
root <hakeemabdullah87@gmail.com>
2024-01-05 16:24:03 -0800

    REFACTOR: applying line break

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,7 +15,7 @@ def find_peak(list_of_integers):
 
     mid = size // 2
     peak = list_of_integers[mid]
-    
+
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
