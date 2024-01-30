4037d8dc9787df205e6e2841118ae9a1df8e4412
root <hakeemabdullah87@gmail.com>
2024-01-30 12:57:49 -0800

    BUGFIX: fixing semistandard error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,18 +2,20 @@
 
 const request = require('request');
 
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const completed = {};
+
+  body.forEach((todo) => {
+    if (todo.completed && !completed[todo.userId]) {
+      completed[todo.userId] = 1;
+    } else {
+      completed[todo.userId] += 1;
+    }
+  });
+  console.log(completed);
 });
