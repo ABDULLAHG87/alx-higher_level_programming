f32732051912816d6ea69dc1cccf6e33449229d3
root <hakeemabdullah87@gmail.com>
2024-02-07 14:11:00 -0800

    BUGFIX: correct spelling error datat to data

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   const url = 'https://www.fourtonfish.com/hellosalut/hello/';
   $('INPUT#btn_translate').click(function () {
     $.get(url + $.param({ lang: $('INPUT#language_code').val() }),
-      function (datat) {
+      function (data) {
         $('DIV#hello').html(data.hello);
       });
   });
